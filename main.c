b9606d7f2e22a7ccbd26e75376969777c99f4d07
sohee0913 <thgml34567@gmail.com>
2022-10-18 17:11:29 +0900

    6-1

--- main.c
+++ main.c
@@ -3,23 +3,14 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void sub(void);
+int all_files;
+
+extern void sub();
 
 int main(int argc, char *argv[]) {
-	int i;
-	for(i=0;i<3;i++)
 	sub();
-	return 0;
+	printf("%d\n", all_files);
+	
+	return 0;	 
 }
 
-void sub(void)
-{
-	int auto_count=0; //자동 할당 
-	static int static_count=0; //정적 할당 
-	auto_count++;
-	static_count++;
-	
-	printf("auto_count=%d\n",auto_count);
-	printf("static_count=%d\n",static_count);
-	 
-}
