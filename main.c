104fbc96a32186dfc17206210da92bd57fbb55e8
sohee0913 <thgml34567@gmail.com>
2022-10-18 16:48:28 +0900

    03

--- main.c
+++ main.c
@@ -5,14 +5,16 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
+	int i=10;
 	
-	for(i=0;i<5;i++)
-	{
-	int temp=1;
-	printf("temp=%d\n", temp);
-	temp++;
+	printf("함수 호출전 i=%d\n", i);
+	inc(i);
+	printf("함수 호출후 i=%d\n", i);
 	
-	}
 	return 0;
+}
+int inc(int counter)
+{
+	counter++;
+	return counter;
 }
