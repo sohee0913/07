884829e4e1c0019700c90017d1576da0549eb2d5
sohee0913 <thgml34567@gmail.com>
2022-10-18 17:20:02 +0900

    6-2

--- main.c
+++ main.c
@@ -3,14 +3,15 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int all_files;
+static int all_files;
 
 extern void sub();
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	sub();
 	printf("%d\n", all_files);
 	
-	return 0;	 
+	return 0;
 }
 
